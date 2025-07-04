369e22e9574e68fc884d045276530f139bf578c6
Amane <vcajdecalul@gmail.com>
2025-07-05 01:52:10 +0700

    chore(ent): update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
+set -e
+
 npx prisma generate
 
-npx prisma db push
-
-npm run dev
+if [ "$NODE_ENV" = "development" ]; then
+  npx prisma db push
+  exec npm run dev
+else
+  npx prisma migrate deploy
+  exec npm run start
+fi
